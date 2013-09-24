46af4551994e3a2419723348af83e9e300265384
Abhijit Gaikwad <agaikwad@marketlive.com>
2013-09-24 01:36:19 -0700

    Dummy login

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['shoppinpal.mobile-menu','myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
+  	    $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/login'});
   }]);
